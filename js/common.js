9a43fa1e4a184414d5a3e9ce82bcc4ea68f855dc
Splintex <eugene.godin@gmail.com>
2014-07-21 15:51:09 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,21 +1,49 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	$(document).on("click", function(){
+		$(".js-popup").hide();
+	});
 
 	console.log($('body').html());
 
-	function divHeight() {
-		var windowHeight = $(window).height();
-		$(".js-main-screen").css({
-			height: windowHeight
-		});
-	}
-	divHeight();
-
-	$(window).resize(function(){
-		divHeight();
+	$(".js-menu-key").on("click", function(event){
+		$(".js-overlay-menu, .js-menu").addClass("is-visible");
+		$(".js-nav").fadeOut(400);
+		return false;
+		event.stopPropagation();
 	});
 
-});
+	$(".js-overlay-menu, .js-menu-close").on("click", function(event){
+		$(".js-overlay-menu, .js-menu").removeClass("is-visible");
+		$(".js-nav").show();
+		return false;
+		event.stopPropagation();
+	});
+
+
+	function fixedNav() {
+		if ($(window).scrollTop()>0) {
+			$("body").addClass("has-fixed-nav");
+		}
+		else {
+			$("body").removeClass("has-fixed-nav");
+		}
+	}
+	fixedNav();
+	$(window).scroll(function(){
+		fixedNav();
+	});
+
+	$(".js-scroll-to").on("click", function(){
+		var section = $(this).attr("data-section");
+		if (section.length > 0) {
+			$('html, body').animate({
+	            scrollTop: ($("."+section)).offset().top-70
+	        }, 500);
+		}
+		
+		return false;
+	});
+
+
+}); 
