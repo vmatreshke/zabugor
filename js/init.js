d9cb19a48c0289a303ae20162143a06d53269a4e
Eugene Godun <eugene.godun@gmail.com>
2014-11-16 16:09:01 +0200

    upd

--- js/init.js
+++ js/init.js
@@ -2,4 +2,5 @@ head.load("js/lib/jquery-1.10.2.js",
 					"js/lib/classie.js",
 					"js/lib/slick.min.js",
 					"js/lib/jquery.inview.min.js",
+					"js/lib/jquery.raty.min.js",
 					"js/common.js");
